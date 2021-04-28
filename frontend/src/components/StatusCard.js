745ee46989e3579d8f9f7f2f5d50663806f88b65
tulathorn <tul.tulathorn@gmail.com>
2021-04-29 00:41:28 +0200

    [ADD]: Modify and delete btn in card

--- frontend/src/components/StatusCard.js
+++ frontend/src/components/StatusCard.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Card } from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 
 const StatusCard = (props) => {
   const { value } = props;
   return (
-    <Card title={value.website_name} style={{ width: 300, margin: 10 }}>
+    <Card
+      title={value.website_name}
+      style={{ width: 300, margin: 10 }}
+      actions={[<EditOutlined key="edit" />, <DeleteOutlined key="delete" />]}
+    >
       <p>
         URL:
-        <a> {value.website_url}</a>
+        <a href={value.website_url}> {value.website_url}</a>
       </p>
       <p>Code: {value.code}</p>
       <p>Status: {value.status}</p>
