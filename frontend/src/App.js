31a8ce7c40b1e765d6dbb333ef7d9eeb6cda0845
tulathorn <tul.tulathorn@gmail.com>
2021-04-29 00:34:18 +0200

    [ADD]: Modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-import { Layout, Button, Row, Col, Card, DatePicker } from "antd";
+import { Layout, Button, Row, Col, Modal } from "antd";
 import StatusCard from "./components/StatusCard";
 
 import "antd/dist/antd.css";
@@ -13,6 +13,8 @@ const App = () => {
   const [webLists, setWebLists] = useState("");
   const url = "http://localhost:4400/api/";
 
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   const getAllWebLists = () => {
     axios
       .get(`${url}status`)
@@ -21,6 +23,18 @@ const App = () => {
         setWebLists(response.data);
       })
       .catch((err) => console.log(`Error ${err}`));
+  };
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
   };
 
   useEffect(() => {
@@ -36,9 +50,13 @@ const App = () => {
         <Content style={{ padding: "50px 50px" }}>
           <div className="site-layout-content">
             <Row justify="space-between">
-              <Col span={8}>Number of source: {"<Response lenght>"}</Col>
+              <Col span={8}>Number of source: {webLists.length}</Col>
               <Col span={2}>
-                <Button type="primary" style={{ marginLeft: 8 }}>
+                <Button
+                  type="primary"
+                  style={{ marginLeft: 8 }}
+                  onClick={showModal}
+                >
                   Add website
                 </Button>
               </Col>
@@ -47,21 +65,6 @@ const App = () => {
               justify="space-around"
               gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
             >
-              {/* <StatusCard />
-              <StatusCard />
-              <StatusCard />
-              <StatusCard />
-              <StatusCard />
-              <StatusCard />
-              <StatusCard />
-              <StatusCard />
-              <StatusCard />
-              <StatusCard />
-              <StatusCard />
-              <StatusCard />
-              <StatusCard />
-              <StatusCard />
-              <StatusCard /> */}
               {webLists ? (
                 webLists.map((element) => (
                   <StatusCard key={element.id.toString()} value={element} />
@@ -71,6 +74,17 @@ const App = () => {
               )}
             </Row>
           </div>
+          <Modal
+            title="Vertically centered modal dialog"
+            centered
+            visible={isModalVisible}
+            onOk={handleOk}
+            onCancel={handleCancel}
+          >
+            <p>some contents...</p>
+            <p>some contents...</p>
+            <p>some contents...</p>
+          </Modal>
         </Content>
       </Layout>
     </div>
