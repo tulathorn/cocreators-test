8cc5bc3e14dc5a824f2ce6e3ecbb0b1e64b3f080
tulathorn <tul.tulathorn@gmail.com>
2021-04-28 23:29:38 +0200

    [ADD]: UI mockup uisng antd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Layout, Button, Row, Col, Card, DatePicker } from "antd";
 
-function App() {
+import "antd/dist/antd.css";
+import "./App.css";
+
+const { Header, Footer, Sider, Content } = Layout;
+const style = { background: "#0092ff", padding: "8px 0" };
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Layout>
+        <Header>
+          <h1 style={{ color: "white" }}>Website monitor</h1>
+        </Header>
+        <Content style={{ padding: "50px 50px" }}>
+          <div className="site-layout-content">
+            <Row justify="space-between">
+              <Col span={8}>Number of source: {"<Response lenght>"}</Col>
+              <Col span={2}>
+                <Button type="primary" style={{ marginLeft: 8 }}>
+                  Add website
+                </Button>
+              </Col>
+            </Row>
+            <Row
+              justify="space-around"
+              gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
+            >
+              <Card title={"website-name"} style={{ width: 300, margin: 10 }}>
+                <p>{"url"}</p>
+                <p>Code: {200}</p>
+                <p>Status: {"ok"}</p>
+              </Card>
+              <Card title="Card" style={{ width: 300, margin: 10 }}>
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
+              <Card title="Card" style={{ width: 300, margin: 10 }}>
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
+              <Card title="Card" style={{ width: 300, margin: 10 }}>
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
+              <Card title="Card" style={{ width: 300, margin: 10 }}>
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
+            </Row>
+          </div>
+        </Content>
+      </Layout>
     </div>
   );
-}
+};
 
 export default App;
