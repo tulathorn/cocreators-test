bf71fc866b56ca0cfaeecbc4417a267b7a37daad
tulathorn <tul.tulathorn@gmail.com>
2021-05-02 17:11:17 +0200

    [EDIT] Port management

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,9 @@ from flask import Flask
 
 from routing import route_bp
 
+PORT = os.getenv('PORT') or 4400
+MODE = os.getenv('FLASK_DEBUG') or False
+
 app = Flask(__name__)
 # CORS(app, resources=r'/*')
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -11,6 +14,4 @@ app.register_blueprint(route_bp, url_prefix='/api')
 
 
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=os.getenv(
-    #     'PORT'), debug=os.getenv('FLASK_DEBUG'))
-    app.run(host="0.0.0.0", port=4400, debug=True)
+    app.run(host="0.0.0.0", port=PORT, debug=MODE)
